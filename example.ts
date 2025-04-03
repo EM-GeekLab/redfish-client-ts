15838cffe2e26ed27952e1b066195b9e34fae970
韩翔宇 <admin@pescn.cn>
2025-04-04 01:52:29 +0800

    feat: add network interface information retrieval and logging

--- example.ts
+++ example.ts
@@ -24,23 +24,40 @@ async function main() {
     for (const systemId of systemIds) {
       console.log(`\n获取系统 ${systemId} 的信息:`);
       startTime = Date.now();
-      const [cpus, memory, pcieDevices] = await Promise.all([
+      const [cpus, memory, pcieDevices, networks] = await Promise.all([
         bmcClient.getCPUInfo(systemId),
         bmcClient.getMemoryInfo(systemId),
-        bmcClient.getPCIeDevicesInfo(systemId)
+        bmcClient.getPCIeDevicesInfo(systemId),
+        bmcClient.getNetworkInterfaceInfo(systemId),
       ]);
       console.log(`\n获取系统基础信息耗时 ${Date.now() - startTime} ms`);
       console.log(JSON.stringify(cpus, null, 2));
       console.log(JSON.stringify(memory, null, 2));
       console.log(JSON.stringify(pcieDevices, null, 2));
+      console.log(JSON.stringify(networks, null, 2));
 
-      // 挂载虚拟光驱并启动
-      startTime = Date.now();
-      const {status: loadSuccess, matchingMedia} = await bmcClient.bootVirtualMedia(mediaUrl, systemId);
-      console.log(`\n挂载虚拟光驱${loadSuccess? "成功": "失败"}, 耗时 ${Date.now() - startTime} ms`);
-      if (loadSuccess) {
-        console.log(JSON.stringify(matchingMedia, null, 2));
+      // 关键能用上的信息
+      console.log(`\n系统ID: ${systemId}`);
+      console.log(`CPU 架构: ${cpus[0].architecture}`);
+      console.log(`全部 MAC 地址与网卡是否连接的对应关系：`);
+      for (const network of networks) {
+        console.log(`\n网卡ID: ${network.id}`);
+        const ports = network.ports || [];
+        for (const port of ports) {
+          console.log(`MAC 地址: ${port.macAddress}, 连接状态: ${port.linkStatus}，链路速度: ${port.speedMbps} Mbps`);
+        }
+        console.log("--------------------\n");
       }
+
+      console.log(`\n=========================================\n`);
+
+      // // 挂载虚拟光驱并启动
+      // startTime = Date.now();
+      // const {status: loadSuccess, matchingMedia} = await bmcClient.bootVirtualMedia(mediaUrl, systemId);
+      // console.log(`\n挂载虚拟光驱${loadSuccess ? "成功" : "失败"}, 耗时 ${Date.now() - startTime} ms`);
+      // if (loadSuccess) {
+      //   console.log(JSON.stringify(matchingMedia, null, 2));
+      // }
     }
     console.log(`\n已退出登录，耗时 ${Date.now() - startTime} ms`);
   } catch (error) {
